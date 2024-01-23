334e190cd1419d8d26193cf5431ecfccb9b9639c
Chirag <chiragmunjal550@gmail.com>
2024-01-23 15:21:37 +0530

    started Admin Dashboard-sidebar/ui

--- client/app/admin/page.tsx
+++ client/app/admin/page.tsx
@@ -1,8 +1,31 @@
 "use client";
 import React from "react";
+import AdminProtected from "../hooks/AdminProtected";
+import Heading from "../utils/Heading";
+import AdminSidebar from "../components/Admin/sidebar/AdminSidebar";
+import DashBoardHero from "../components/Admin/DashBoardHero";
 
 const page = () => {
-  return <div>Chirag</div>;
+  return (
+    <div>
+      <AdminProtected>
+        <Heading
+          title={`Elearning-Admin`}
+          description="Elearning is a platform for students to learn and get help from teachers"
+          keywords="Programming , MERN ,REDUX , Machine Learning"
+        />
+        <div className="flex h-[200vh]">
+          {" "}
+          <div className="1500px:w-[15%] w-1/5">
+            <AdminSidebar />
+          </div>
+          <div className="w-[85%]">
+            <DashBoardHero />
+          </div>
+        </div>
+      </AdminProtected>
+    </div>
+  );
 };
 
 export default page;
---
+++ client/app/components/Admin/DashBoardHero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DashboardHeader from "./DashBoardHeader";
+
+const DashBoardHero = () => {
+  return (
+    <div>
+      <DashboardHeader />
+    </div>
+  );
+};
+
+export default DashBoardHero;
